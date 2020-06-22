3181894f443809f8ad9f79091e9578c2ec79a469
Sanam Malik <sanam.malik04@gmail.com>
2020-06-22 22:35:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,35 +1,31 @@
 def addition(num1, num2)
-sum = num1+num2
+sum = num1 + num2
 puts sum
 sum 
 end
 
 def subtraction(num1, num2)
-sum = num1-num2
+sum = num1 - num2
 puts sum
 sum 
 end
 
 def division(num1, num2)
-sum = num1/num2
+sum = num1 / num2
 puts sum
 sum
 end
 
 def multiplication(num1, num2)
-sum = num1*num2
+sum = num1 * num2
 puts sum 
 sum 
 end
 
 def modulo(num1, num2)
-sum = num1%num2
-puts sum 
-sum 
+
 end
 
 def square_root(num)
-sum = **num
-puts sum 
-sum 
+
 end
